daf3ac796211a03f2945a8ccf825025bd5abf505
malshakolambage <malshakolambage20@gmail.com>
2026-05-13 16:51:01 +0530

    Initial Flutter project setup

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("boltANDgears")),
+        body: Center(
+          child: Text("Hello Flutter 🚀", style: TextStyle(fontSize: 30)),
+        ),
+      ),
+    );
+  }
+}
